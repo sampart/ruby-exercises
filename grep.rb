3d8127141d6c24768e37ccdbff2babdd9a15bf12
Sam Partington <spartington@semmle.com>
2019-10-30 11:53:51 +0000

    Accept search term as argument

--- grep.rb
+++ grep.rb
@@ -1,4 +1,9 @@
+if ARGV.length != 1
+  puts "Please specify one string to search for"
+  exit
+end
+
 contents = File.read('file.txt')
 contents.split(/\r?\n/).each do |line|
-  puts line if line.match?(/.*monkey.*/)
+  puts line if line.match?(/.*#{Regexp.quote(ARGV[0])}.*/)
 end
